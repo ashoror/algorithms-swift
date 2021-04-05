fab35fa7a15729a0172f38ee8a898558370d3f47
ashoror <fallen.frostful@gmail.com>
2021-04-05 10:54:05 +0300

    [Array][RemoveDuplicatesSortedArray] Remove Duplicates from Sorted Array

---
+++ Array/RemoveDuplicatesSortedArray.swift
@@ -0,0 +1,35 @@
+/// Remove Duplicates from Sorted Array
+/// https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3248/
+///
+/// Example 1:
+/// `Input: nums = [1,1,2]`
+/// `Output: 2, nums = [1,2]`
+///` Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.`
+///
+/// Example 2:
+/// `Input: nums = [0,0,1,1,1,2,2,3,3,4]`
+/// `Output: 5, nums = [0,1,2,3,4]`
+/// `Explanation: Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively. It doesn't matter what values are set beyond the returned length.`
+///
+/// Constraints:
+/// `0 <= nums.length <= 3 * 104`
+/// `-104 <= nums[i] <= 104`
+///`nums is sorted in ascending order.`
+
+struct Solution {
+
+	/// Time: O(N); Space: O(1)
+	func removeDuplicates(_ nums: inout [Int]) -> Int {
+		if nums.count == 0 { return 0 }
+
+		var index: Int = 0
+		for i in 0..<nums.count {
+			if nums[index] != nums[i] {
+				index += 1
+				nums[index] = nums[i]
+			}
+		}
+
+		return index + 1
+	}
+}
